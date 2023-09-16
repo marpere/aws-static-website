1d032421133e4d8826c19ae5022c767318c1281e
Marcus Pereira <max171@gmail.com>
2023-09-16 13:30:57 -0300

    Add acl

--- without-cdn/storage.tf
+++ without-cdn/storage.tf
@@ -41,6 +41,11 @@ resource "aws_s3_bucket_public_access_block" "website" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_acl" "website" {
+  bucket = aws_s3_bucket.website.id
+  acl    = "public-read"
+}
+
 resource "aws_s3_bucket_policy" "allow_access_from_cloudflare" {
   bucket = aws_s3_bucket.website.id
   policy = data.aws_iam_policy_document.allow_access_from_cloudflare.json
