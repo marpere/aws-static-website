6d24f442b9130a799a6bcddd0d64359a27ff85c1
Marcus Pereira <max171@gmail.com>
2023-09-16 13:21:44 -0300

    Add public block access

--- without-cdn/storage.tf
+++ without-cdn/storage.tf
@@ -33,6 +33,14 @@ resource "aws_s3_object" "website" {
   etag         = filemd5("${var.static_dir}${each.key}")
 }
 
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "allow_access_from_cloudflare" {
   bucket = aws_s3_bucket.website.id
   policy = data.aws_iam_policy_document.allow_access_from_cloudflare.json
